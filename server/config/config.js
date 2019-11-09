f387de41b9f877914a363fc55aff3497fc23c29a
dpadillaf <dpadillafonseca@gmail.com>
2019-11-09 10:48:03 -0500

    Add variables de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://falkry:padilla123@cluster0-vmhze.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
